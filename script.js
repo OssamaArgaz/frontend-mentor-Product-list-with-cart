a8987716048f6e827d5887cbdae918572550891f
ossama argaz <ossama2323>
2025-04-06 10:24:04 +0100

    terminer le travaille sur la partie des cards et debus sur la partie de l'ajoute des produit et calculation de total

--- script.js
+++ script.js
@@ -3,22 +3,31 @@ document.querySelectorAll('.add-to-cart-btn').forEach(addToCartBtn =>{
     let iconPlus = addToCartBtn.parentElement.nextElementSibling.firstElementChild.lastElementChild;
     let iconMoins = addToCartBtn.parentElement.nextElementSibling.firstElementChild.firstElementChild;
     let product = iconPlus.previousElementSibling;
+    let cartActive = document.querySelector('.cart-active');
+    let cartImg = document.querySelector('.cart-img');
+    let totalProducts = document.getElementById('totalProducts')
 
     addToCartBtn.addEventListener('click', function showBtnActive() {
         addToCartBtn.parentElement.nextElementSibling.classList.add('d-block')
         addToCartBtn.parentElement.classList.add('d-none');
 
+        cartActive.classList.remove('d-none')
+        cartImg.classList.add('d-none');
+
         quantity += 1;
         product.innerHTML = quantity;
 
-        addProductsCart()
+        addProductsNumberCart()
+        console.log(this)
     });
 
     iconPlus.addEventListener('click', function addProduct(){
         let product = iconPlus.previousElementSibling;
         quantity += 1;
         product.innerHTML = quantity;
-        addProductsCart()
+        addProductsNumberCart()
+        console.log(this)
+
     });
 
     iconMoins.addEventListener('click', function addProduct(){
@@ -29,12 +38,18 @@ document.querySelectorAll('.add-to-cart-btn').forEach(addToCartBtn =>{
             addToCartBtn.parentElement.nextElementSibling.classList.remove('d-block');
             addToCartBtn.parentElement.classList.remove('d-none');
         }
-        removeProductsCart()
+        removeProductsNumberCart()
+        if(totalProducts.textContent == 0){
+            cartActive.classList.add('d-none')
+            cartImg.classList.remove('d-none');
+        }
     });
+
+
 
 })
 
-function addProductsCart(){
+function addProductsNumberCart(){
     let total = 0;
     let totalProducts = document.getElementById('totalProducts');
     document.querySelectorAll('.icofont-minus-circle').forEach(minusIcon =>{
@@ -44,7 +59,7 @@ function addProductsCart(){
     })
 }
 
-function removeProductsCart(){
+function removeProductsNumberCart(){
     let total = 0;
     let totalProducts = document.getElementById('totalProducts');
     document.querySelectorAll('.icofont-minus-circle').forEach(minusIcon =>{
@@ -53,3 +68,18 @@ function removeProductsCart(){
         totalProducts.innerHTML = Math.abs(total);
     })
 }
+
+function addProductToCart(){
+    document.querySelectorAll('.add-to-cart-btn-active span').forEach(span =>{
+        let cartActive = document.querySelector('.cart-active');
+        let cartImg = document.querySelector('.cart-img');
+        
+        if(span.textContent == 0){
+            cartActive.classList.add('d-none')
+            cartImg.classList.remove('d-none');
+        }
+        console.log(span.textContent)
+    })
+}
+
+
